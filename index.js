b83c35a349dda6be8fdf631755438fc3e1a7df03
Mario Fernandez <diddleslip@gmail.com>
2024-02-13 16:36:29 -0500

    Documented JS file! Removed old classes in CSS file.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+/* ----------
+
+- scrollFunction runs everytime the user scrolls
+- Scrolling down 30px resizes the navbar's padding
+
+---------- */
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementsByClassName("navBarUL")[0].style.paddingTop = "10px";
   } else {
     // Access the first element with class "navBarUL" and set its padding
@@ -14,52 +19,59 @@ function scrollFunction() {
 
 /* ----------
 
-- typingAnimationFunction
+- typingAnimationFunction reconstructs the element with <spans> of the element's length
+- Checking header type to assign font-family and CSS
+- After a tiny delay changes the opacity from 0 to 1 of each letter
+- Creates a 'typing' effect
+- Cleans up span tags with the original content for easier load on memory
 
 ---------- */
-const typingObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const element = entry.target;
-      const text = element.textContent; // Get the text content of the element
-      element.textContent = ""; // Clear the element content
+const typingObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const element = entry.target;
+        const text = element.textContent; // Get the text content of the element
+        element.textContent = ""; // Clear the element content
 
-      // Create a span for each character in the text
-      for (let i = 0; i < text.length; i++) {
-        let span = document.createElement("span");
-        span.textContent = text[i];
-        span.style.opacity = "0"; // Start with the character hidden
-        element.appendChild(span);
-      }
+        // Create a span for each character in the text
+        for (let i = 0; i < text.length; i++) {
+          let span = document.createElement("span");
+          span.textContent = text[i];
+          span.style.opacity = "0"; // Start with the character hidden
+          element.appendChild(span);
+        }
 
-      // Reveal each character with a delay
-      const spans = element.querySelectorAll("span");
-      spans.forEach((span, index) => {
+        // Reveal each character with a delay
+        const spans = element.querySelectorAll("span");
+        spans.forEach((span, index) => {
+          setTimeout(() => {
+            if (
+              (element.tagName.toLowerCase() == "h1") |
+              (element.tagName.toLowerCase() == "h2")
+            ) {
+              span.style.fontFamily = '"Alliance No. 2"';
+            }
+
+            if (element.tagName.toLowerCase() == "h3") {
+              span.style.fontFamily = '"Alliance No. 1"';
+            }
+            span.style.opacity = "1"; // Make the character visible
+            span.style.color = "#1e2124"; // Change to the desired color
+          }, index * 25); // Adjust the delay as needed
+        });
+
         setTimeout(() => {
-          if (
-            (element.tagName.toLowerCase() == "h1") |
-            (element.tagName.toLowerCase() == "h2")
-          ) {
-            span.style.fontFamily = '"Alliance No. 2"';
-          }
+          element.textContent = text;
+          element.style.color = "#1e2124";
+        }, 5000);
 
-          if (element.tagName.toLowerCase() == "h3") {
-            span.style.fontFamily = '"Alliance No. 1"';
-          }
-          span.style.opacity = "1"; // Make the character visible
-          span.style.color = "#1e2124"; // Change to the desired color
-        }, index * 25); // Adjust the delay as needed
-      });
-
-      setTimeout(() => {
-        element.textContent = text;
-        element.style.color = "#1e2124";
-      }, 5000);
-
-      typingObserver.unobserve(entry.target);
-    }
-  });
-});
+        typingObserver.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 } // How much in page before applying class
+);
 
 const typingAnimationFunction = document.querySelectorAll(".animationTyping");
 typingAnimationFunction.forEach((el) => typingObserver.observe(el));
@@ -67,7 +79,7 @@ typingAnimationFunction.forEach((el) => typingObserver.observe(el));
 /* ----------
 
 - animationBorderFunction selects all the .animationBorder classes in the DOM.
-- Showing the element adds the class 'animated'
+- Showing the element adds the class 'animationBorderDONE'
 - Happens once per element
 - Allowing for the animation to essentially be 'attached'
 
